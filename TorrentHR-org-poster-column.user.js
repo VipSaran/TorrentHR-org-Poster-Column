33e638a674b19ef5497860bf3cebaab5ec2f6426
Robert Saric <robert.saric@gmail.com>
2019-06-04 22:06:24 +0200

    initial

---
+++ TorrentHR-org-poster-column.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          TorrentHR.org Poster Column
+// @description   Greasemonkey/Tampermonkey UserScript for extending TorrentHR.org Torrents page with additional poster column
+// @namespace     http://github.com/VipSaran/TorrentHR-org-Poster-Column
+// @updateURL     https://github.com/VipSaran/TorrentHR-org-Poster-Column/raw/master/TorrentHR-org-poster-column.user.js
+// @version       1.0.0
+// @author        VipSaran
+// @require       http://code.jquery.com/jquery-3.4.1.min.js
+// @grant         GM_getResourceURL
+// @include       https://www.torrenthr.org/browse.php*
+// @include       http://www.torrenthr.org/browse.php*
+// @match         https://www.torrenthr.org/browse.php*
+// @match         http://www.torrenthr.org/browse.php*
+// @run-at        document-end
+// ==/UserScript==
